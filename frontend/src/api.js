7e8bfd30a5b3c5b2936181c40805d958252c4ddd
Angel Valadez <valadezhdzangb@gmail.com>
2025-03-09 08:27:42 -0600

    Ultimo commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/songs';
+const API_URL = 'https://backendsongs-93gj.onrender.com/api/songs';
 
 export const getSongs = async () => {
     const response = await axios.get(API_URL);
